c7d2e37cddb6646e7afc96ab44203f05ee3cf270
RAJA143 <RAJA143@DESKTOP-21PKQEC>
2022-10-19 11:50:54 +0530

    tttttddg

---
+++ Raja000/src/test/java/raja/pro/RAJAq.java
@@ -0,0 +1,15 @@
+package raja.pro;
+
+public class RAJAq {
+
+	public static void main(String[] args) {
+
+			String r="RAJA";
+			String b="RANI";
+			String ra=r+b;
+			System.out.println(ra);
+
+		}
+
+	}
+
